2e59fd66a8cdbe8e6f582cd058017b0962e41101
NicolasP <pistillonicolas@gmail.com>
2021-03-13 15:18:01 -0300

    Avances en funcionalidad de login

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,14 +7,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasTeams;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use HasApiTokens;
     use HasFactory;
-    use HasTeams;
+    use HasRoles;
     use Notifiable;
     use TwoFactorAuthenticatable;
 
